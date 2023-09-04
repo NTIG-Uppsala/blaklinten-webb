9ef2ef0efaefefe0ec0aa4d0ac6b54d1d474c9c6
Simon Clavensjö <simon.clavensjo@elev.ntig.se>
2023-09-04 13:29:09 +0200

    Fix social media tests
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
    Co-Authored-By: Andreas423 <142991086+Andreas423@users.noreply.github.com>

--- tests/test.py
+++ tests/test.py
@@ -1,49 +1,62 @@
-from os import path, getcwd
+import time
+from os import getcwd, path
 from unittest import TestCase, main
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 
 class Tests(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.browser = webdriver.Chrome()
 
-    @classmethod
-    def tearDownClass(cls):
-        pass
-
     def setUp(self):
         self.browser.get(path.join(getcwd(), "index.html"))
 
     def tearDown(self):
-        self.browser.get('about:blank')
+        self.browser.get("about:blank")
 
     def test_company_name_present(self):
         self.assertIn("Florist Blåklinten", self.browser.page_source)
 
     def test_footer(self):
         self.browser.find_element(By.TAG_NAME, "footer")
-    
+
+    def scroll_to_bottom(self):
+        self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.5)
+
     def test_instagram_link(self):
+        self.scroll_to_bottom()
         self.browser.find_element(By.ID, "instagram-link").click()
-        self.assertEqual("https://www.instagram.com/ntiuppsala/", self.browser.current_url)
+        self.assertEqual(
+            "https://www.instagram.com/ntiuppsala/", self.browser.current_url
+        )
 
     def test_twitter_link(self):
+        self.scroll_to_bottom()
         self.browser.find_element(By.ID, "x-link").click()
         self.assertIn("https://twitter.com", self.browser.current_url)
         self.assertIn("ntiuppsala", self.browser.current_url)
 
     def test_facebook_link(self):
+        self.scroll_to_bottom()
         self.browser.find_element(By.ID, "facebook-link").click()
-        self.assertEqual("https://www.facebook.com/ntiuppsala", self.browser.current_url)
+        self.assertEqual(
+            "https://www.facebook.com/ntiuppsala", self.browser.current_url
+        )
 
     def test_phone_number_present(self):
-        phone_number_link = self.browser.find_element(By.XPATH, "//a[@href='tel:0630-555-555']")
+        phone_number_link = self.browser.find_element(
+            By.XPATH, "//a[@href='tel:0630-555-555']"
+        )
         self.assertIn("0630-555-555", phone_number_link.text)
-    
+
     def test_email_address_present(self):
-        email_address_link = self.browser.find_element(By.XPATH, "//a[@href='mailto:info@ntig-uppsala.github.io']")
+        email_address_link = self.browser.find_element(
+            By.XPATH, "//a[@href='mailto:info@ntig-uppsala.github.io']"
+        )
         self.assertIn("info@ntig-uppsala.github.io", email_address_link.text)
 
     def test_address_present(self):
@@ -57,6 +70,7 @@ class Tests(TestCase):
 
     def test_closed_days_present(self):
         self.assertIn("Stängda dagar", self.browser.page_source)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main(verbosity=2)
