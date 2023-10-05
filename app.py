a80e286881f7ece420ea3cd84c25d655b9bcfb94
Carl595 <105862361+Carl595@users.noreply.github.com>
2023-10-05 10:42:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,14 @@ from flask import (
     send_from_directory,
     url_for,
 )
-from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 
-mysql = MySQL(app)
-
-
 @app.route("/")
 def index():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM din_tabell")
-    data = cur.fetchall()
-    cur.close()
-    return render_template("index.html", data=data)
+    print("Request for index page received")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
